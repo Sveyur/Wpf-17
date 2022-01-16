e08aac4da844df8eca2a0cb6495f077c196f107d
smolkovasg@yandex.ru <smolkovasg@yandex.ru>
2022-01-16 13:20:21 +0300

    Добавьте файлы проекта.

---
+++ Wpf-17/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_17
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void colorPicker_ColorChanged(object sender, RoutedPropertyChangedEventArgs e)
+        {
+            txb.Text = e.NewValue.ToString();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+        
+    }
+}
